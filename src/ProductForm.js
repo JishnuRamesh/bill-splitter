6a7cf662556d6086efd3b8160fb050f1ff78e63f
Jishnu <jish_r@live.com>
2019-01-29 17:00:11 +1100

    Added first image

--- src/ProductForm.js
+++ src/ProductForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import jish from './images/jish.jpg';
 
 
 
@@ -16,7 +17,7 @@ class ProductForm extends Component {
             
             
 
-                <h5 className="text-center"> Add Product </h5>
+                <h5 className="text-center mt-2"> Add Product </h5>
 
 
                 <div className="inputSet">
@@ -29,14 +30,29 @@ class ProductForm extends Component {
 
                      <div className="form-group col-md-8">
                           <label htmlFor="product">Product (Optional)</label>
-                       <input type="text" className="form-control" id="product" placeholder="ProductName"  />
+                       <input type="text" className="form-control" id="product" placeholder="ProductName"  required />
                      </div>
 
                      <div className="form-group col-md-4">
                        <label htmlFor="amount">Amount</label>
-                        <input type="number" className="form-control" id="amounbt" placeholder="$0.0" />
+                        <input type="number" className="form-control" id="amounbt" placeholder="$0.0"  required/>
                       </div>
                 </div>
+
+
+                 <h5 className="text-center p-2"> Select whom you want to split the bills with </h5>
+
+                <div className="form-row">
+
+                     <div className="form-group col-md-12">
+                        <img className="img-fluid rounded-circle" src={jish} />
+
+                     </div>
+
+                     
+
+                </div>
+             
              
 
 
