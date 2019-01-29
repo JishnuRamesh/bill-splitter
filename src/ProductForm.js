c7f455927eee12aaa4ea67c44f17f698c2049eb6
Jishnu <jish_r@live.com>
2019-01-29 16:02:39 +1100

    Chnaged the place holder in forms

--- src/ProductForm.js
+++ src/ProductForm.js
@@ -20,21 +20,27 @@ class ProductForm extends Component {
 
 
                 <div className="inputSet">
+
+
+                <form>
              
              
                 <div className="form-row">
 
                      <div className="form-group col-md-8">
-                          <label htmlFor="inputEmail4">Email</label>
-                       <input type="email" className="form-control" id="inputEmail4" placeholder="ProductName"  />
+                          <label htmlFor="product">Product (Optional)</label>
+                       <input type="text" className="form-control" id="product" placeholder="ProductName"  />
                      </div>
 
                      <div className="form-group col-md-4">
-                       <label htmlFor="inputPassword4">Password</label>
-                        <input type="password" className="form-control" id="inputPassword4" placeholder="Password" />
+                       <label htmlFor="amount">Amount</label>
+                        <input type="number" className="form-control" id="amounbt" placeholder="$0.0" />
                       </div>
                 </div>
              
+
+
+                </form>
              
              </div>
              
