ed91352083c73ac307d201d0d7d765bae16f6367
Jishnu Ramesh <Jish_r@live.com>
2019-02-03 10:04:03 +1100

    Updates

---
+++ precache-manifest.5c9dce37c1c3ccaeb16e785fa14f74ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/JishnuRamesh/bill-splitter/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "e4d60ff7d30e247230bdd303dbb232a1",
+    "url": "/JishnuRamesh/bill-splitter/static/media/logo.e4d60ff7.png"
+  },
+  {
+    "revision": "78f3656702166a65c17d",
+    "url": "/JishnuRamesh/bill-splitter/static/js/runtime~main.78f36567.js"
+  },
+  {
+    "revision": "f509fdfacafc08c2cfe0",
+    "url": "/JishnuRamesh/bill-splitter/static/js/main.f509fdfa.chunk.js"
+  },
+  {
+    "revision": "62d7e86bc4e96464cd43",
+    "url": "/JishnuRamesh/bill-splitter/static/js/1.62d7e86b.chunk.js"
+  },
+  {
+    "revision": "f509fdfacafc08c2cfe0",
+    "url": "/JishnuRamesh/bill-splitter/static/css/main.5017bd4a.chunk.css"
+  },
+  {
+    "revision": "62d7e86bc4e96464cd43",
+    "url": "/JishnuRamesh/bill-splitter/static/css/1.40eacbf2.chunk.css"
+  },
+  {
+    "revision": "811040326278d66a946bf17fc3ed62fe",
+    "url": "/JishnuRamesh/bill-splitter/index.html"
+  }
+];
